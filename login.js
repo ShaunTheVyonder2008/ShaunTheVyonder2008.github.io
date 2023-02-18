b682de3cbc1732a9d93525ea8b187a67e738c053
SSP6904 <hoffershaun842@gmail.com>
2023-02-18 14:34:47 -0500

    [EDIT] login.js, NULL problem

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
-
-loginButton.addEventListener("click", (e) => {
+if (loginButton) {
+  loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
@@ -10,8 +10,9 @@ loginButton.addEventListener("click", (e) => {
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in. \n Returning to Home Page");
         location.href("/");
-    } else {
+     } else {
         alert ("Your password is not correct or is not found on the database!");
         location.reload();
-    }
-})
+     }
+  }); 
+}
