a7ce2c51058db30f343c1898f565a117d4f60d8d
SSP6904 <hoffershaun842@gmail.com>
2023-02-19 22:24:01 -0500

    [EDIT, ADD] Major changes

--- login.js
+++ login.js
@@ -1,17 +1,24 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
-if (loginButton) {
-  loginButton.addEventListener('click', () => {
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+	function validate(form){
+		if(form.username.value.length==0){
+			alert("You did not enter a username or password! \n Try again!");
+			form.username.focus();
+			return false;
+        } 
+        if(form.userpassword.value.length==0){
+			alert("You did not enter a username or password! \n Try again!");
+			form.userpassword.focus();
+			return false;
+        } 
+        if(form.username.value.length==9){
+            alert('You are logged to TTNRT!');
+            form.username.focus();
+            return false;
+        }
+        if(form.userpassword.value.length==8){
+            alert('You are logged to TTNRT!');
+            form.userpassword.focus();
+            return false;
+        }
+        return true;     
+    }    
 
-    if (username === "shaun" && password === "admin") {
-        alert('You have successfully logged in. \n Returning to Home Page');
-        location.href('/');
-     } else {
-        alert ('Your password is not correct or is not found on the database!');
-        location.reload();
-     }
-  }); 
-}
