c213add3726637c4c87a22333e8ce07b1bd26649
SSP6904 <hoffershaun842@gmail.com>
2023-03-17 17:36:04 -0400

    Meh

--- console.js
+++ console.js
@@ -23,34 +23,9 @@ console.log('....................................');
 console.log('....................................');
 
 // Extra Code
-function drawShape() {
-            
-    // get the canvas element using the DOM
-    var canvas = document.getElementById('mycanvas');
-    
-    // Make sure we don't execute when canvas isn't supported
-    if (canvas.getContext) {
-    
-       // use getContext to use the canvas for drawing
-       var ctx = canvas.getContext('2d');
-       
-       // Draw shapes
-       var img = new Image();
-       img.src = '/images/logo.png';
-       
-       img.onload = function() {
-          ctx.drawImage(img,0,0);
-          ctx.beginPath();
-          
-          ctx.moveTo(30,96);
-          ctx.lineTo(70,66);
-          
-          ctx.lineTo(103,76);
-          ctx.lineTo(170,15);
-          
-          ctx.stroke();
-       }
-    } else {
-       alert('You need Safari or Firefox 1.5+ to see this demo.');
-    }
- }
+window.onload = function() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("logo");
+    ctx.drawImage(img, 0, -40);
+  }
