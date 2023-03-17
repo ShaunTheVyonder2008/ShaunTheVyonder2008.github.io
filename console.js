73e13d7aea1cad656ad4ec218764e3fdfba01516
SSP6904 <hoffershaun842@gmail.com>
2023-03-17 16:29:45 -0400

    NO

--- console.js
+++ console.js
@@ -22,7 +22,35 @@ console.log('....................................');
 console.log('....................................');
 console.log('....................................');
 
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
+// Extra Code
+function drawShape() {
+            
+    // get the canvas element using the DOM
+    var canvas = document.getElementById('myCanvas');
+    
+    // Make sure we don't execute when canvas isn't supported
+    if (canvas.getContext) {
+    
+       // use getContext to use the canvas for drawing
+       var ctx = canvas.getContext('2d');
+       
+       // Draw shapes
+       var img = new Image();
+       img.src = '/images/logo.png';
+       
+       img.onload = function() {
+          ctx.drawImage(img,0,0);
+          ctx.beginPath();
+          
+          ctx.moveTo(30,96);
+          ctx.lineTo(70,66);
+          
+          ctx.lineTo(103,76);
+          ctx.lineTo(170,15);
+          
+          ctx.stroke();
+       }
+    } else {
+       alert('You need Safari or Firefox 1.5+ to see this demo.');
+    }
+ }
