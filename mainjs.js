78c3ef75a77360b5051a11b57f1fae5cdd8baf04
SSP6904 <hoffershaun842@gmail.com>
2023-03-17 17:52:25 -0400

    II

--- mainjs.js
+++ mainjs.js
@@ -1,19 +1,12 @@
 // Console log
 // In javascript!
 
-console.log('----------------');
-console.log('| ttnrtsite.me |');
-console.log('| (C) TTNT CRP |');
-console.log('| 2018 - 2023  |');
-console.log('----------------');
-
-
-// Canvas for image loading!
-window.onload = function() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    var img = document.getElementById("logo");
-    ctx.drawImage(img, 0, -40);
-}
+console.log('www.ttnrtsite.me');
+console.log('(C) TTNRT CORP');
+console.log(' 2018 - 2023');
+console.log('.............');
+console.log('Hey! If you just open this here');
+console.log('dont copy or paste anything here unless told so!');
+console.log('Thanks for understanding!');
 
 // End of line
