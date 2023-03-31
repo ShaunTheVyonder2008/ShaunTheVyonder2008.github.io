47dc49d50948ea5e062f8ae19b169486928531dc
SSP6904 <75172901+SSP6904@users.noreply.github.com>
2023-03-31 18:03:56 -0400

    [EDIT, DEL, RE] undefined

--- redirect1/redirect.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    // Handler for .ready() called.
-    window.setTimeout(function () {
-        location.href = "https://github.com/SSP6904";
-    }, 5000);
-});
--- redirect3/redirect.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    // Handler for .ready() called.
-    window.setTimeout(function () {
-        location.href = "https://github.com/orgs/TTNRT/discussions";
-    }, 5000);
-});
