6748fb6871bc45b463f63d7beba83163a381f870
SSP6904 <75172901+SSP6904@users.noreply.github.com>
2023-04-09 13:11:36 -0400

    unk

--- mainjs.js
+++ mainjs.js
@@ -2,7 +2,7 @@
 // In javascript!
 
 console.log(
-    "%cTTNRT CORP\n%cHey! If someone told you to paste something here, there's a 200% chance you're being scammed.\n%cThanks for understanding!.\n%cmailto:hoffershaun842@gmail.com",
+    "%cTTNRT CORP\n%cHey! If someone told you to paste something here, there's a 200% chance you're being scammed.\n%cThanks for understanding!.\n%choffershaun842@gmail.com",
     "color:blue;font-size:48px;font-weight:600;",
     "color:red;font-size:20px;",
     "color:green;font-size:12px;",
