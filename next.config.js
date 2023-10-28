bbd46b3aa7de0385ae37310b0ede6cab3b31c929
Shaun Hoffer <hoffershaun842@gmail.com>
2023-10-28 18:24:22 -0400

    gg

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      loader: 'akamai',
-      path: '',
-    },
-    assetPrefix: './',
+    basePath: '/ttnrt.github.io',
+    assetPrefix: '/ttnrt.github.io/', // assetPrefix requires the trailing slash
 };
 
 module.exports = nextConfig
