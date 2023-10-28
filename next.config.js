ee4697b1e4b3519d02d0a530190a6947a9e9e1c4
Shaun Hoffer <hoffershaun842@gmail.com>
2023-10-28 18:26:29 -0400

    cf

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     basePath: '/ttnrt.github.io',
     assetPrefix: '/ttnrt.github.io/', // assetPrefix requires the trailing slash
 };
