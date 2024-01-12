61ad4f058d485c40184613fbfdf6c735e9aaab7a
Shaun Hoffer <hoffershaun842@gmail.com>
2024-01-12 13:55:58 +0000

    New changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,5 +12,8 @@ module.exports = {
 		"2xl": "1536px",
 	  },
 	},
-	plugins: [require("@tailwindcss/typography")],
+	plugins: [
+		require("@tailwindcss/typography"),
+		require("daisyui")
+	],
   };
